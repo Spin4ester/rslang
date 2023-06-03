f413a365243ff65cfee5267671f041dbd64e917c
Spin4ester <bakibayev.r@gmail.com>
2023-06-03 23:37:14 +0200

    fix: redeploy backend

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ const config = {
   // sitePath: '/dist', // для npm run build
   sitePath: '/rslang', // для дэплоя на гитхаб
   api: {
-    url: 'https://rs-lang-syllabus.herokuapp.com/',
+    url: 'https://react-rslang-be-production-7ee4.up.railway.app/',
     // url: 'http://localhost:4000/',
   },
 };
